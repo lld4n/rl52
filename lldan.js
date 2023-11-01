3156d30f449c5f70102ed550d2034d8373d14e7c
lldan <safinildan@gmail.com>
2023-11-01 09:51:45 +0300

    Nov 1, 2023, 9:51 AM

---
+++ lldan.js
@@ -0,0 +1,12 @@
+import fs from "fs";
+
+const throws = [".obsidian", "images", "index.md", "templates"]
+const dirs = fs.readdirSync("./content");
+let result = "---\ntitle: rl52\n---\n\n# Дорогая грусть, прости, не вернусь\n\n> я думал уже скип\n\n"
+for (let el of dirs) {
+  if (!throws.includes(el)) {
+    const dir = fs.readdirSync("./content/" + el).filter(el => el !== "index.md")
+    result += `- [[${el}]]\tКоличество - ${dir.length}\n`
+  }
+}
+fs.writeFileSync("./content/index.md", result)
