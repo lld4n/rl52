18dfa049dbd9b1d00f2c2fbfa705f118addf03fb
lldan <safinildan@gmail.com>
2023-12-25 16:10:42 +0300

    Dec 25, 2023, 4:10 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,28 +1,14 @@
 import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/footer.scss"
-import { version } from "../../package.json"
 
-interface Options {
-  links: Record<string, string>
-}
-
-export default ((opts?: Options) => {
+export default (() => {
   function Footer({ displayClass }: QuartzComponentProps) {
-    const year = new Date().getFullYear()
-    const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
         <hr />
         <p>
-          Created with <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a>, © {year}
+          Created by <a href="https://lldan.ru">lldan</a>
         </p>
-        <ul>
-          {Object.entries(links).map(([text, link]) => (
-            <li>
-              <a href={link}>{text}</a>
-            </li>
-          ))}
-        </ul>
       </footer>
     )
   }
